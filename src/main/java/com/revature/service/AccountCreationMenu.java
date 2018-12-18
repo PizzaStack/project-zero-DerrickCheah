3df9f99d8d40969ce452946f7da4a5728e366bd9
Derrick Cheah <derrick.cheah01@gmail.com>
2018-12-17 18:42:22 -0600

    Started working on SQL stuff.

--- src/main/java/com/revature/service/AccountCreationMenu.java
+++ src/main/java/com/revature/service/AccountCreationMenu.java
@@ -4,6 +4,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 import com.revature.entity.Account;
@@ -17,30 +21,25 @@ public class AccountCreationMenu {
 	private String password;
 	private Account accountType;
 	private BankAccount bankAccount;
-	private String folderLocation = "C:\\Users\\Derrick\\Documents\\Revature\\project-zero-DerrickCheah\\TextFiles\\";
-	private File usernames = new File(folderLocation + "Usernames.txt");
-	private File passwords = new File(folderLocation + "Passwords.txt");
-	private File checkingAccount = new File(folderLocation + "CheckingAccounts.txt");
-	private File savingsAccount = new File(folderLocation + "SavingsAccount.txt");
-	private File jointAccount = new File(folderLocation + "JointAccounts.txt");
+	private final String url = "jdbc:postgresql://baasu.db.elephantsql.com:5432/mjlaopyp";
+	private final String user = "mjlaopyp";
+	private final String pass = "JSW85gu3KkXAtrdNU20iAbZaVHRgiE6G";
 
 	public AccountCreationMenu() {
-		try (PrintStream usernamesPS = new PrintStream(new FileOutputStream(usernames, true));
-				PrintStream passwordsPS = new PrintStream(new FileOutputStream(passwords, true));
-				PrintStream checkingPS = new PrintStream(new FileOutputStream(checkingAccount, true));
-				PrintStream savingsPS = new PrintStream(new FileOutputStream(savingsAccount, true));
-				PrintStream jointPS = new PrintStream(new FileOutputStream(jointAccount, true));) {
 
+		try {
+			
+			Connection connection = DriverManager.getConnection(url, user, pass);
+			Statement statement = connection.createStatement();
+			
 			Scanner input = new Scanner(System.in);
 			System.out.print("Please enter a unique username: ");
 			this.customerId = input.nextLine();
-			usernamesPS.println(this.customerId);
 
 			System.out.println();
 
 			System.out.print("Please enter a password: ");
 			this.password = input.nextLine();
-			passwordsPS.println(this.password);
 
 			System.out.println();
 
@@ -55,21 +54,12 @@ public class AccountCreationMenu {
 			do {
 				if (choice.equals("1") || choice.equals("checking") || choice.equals("checking account")) {
 					this.accountType = Account.CHECKING;
-					checkingPS.println("1");
-					savingsPS.println("0");
-					jointPS.println("0");
 					b = false;
 				} else if (choice.equals("2") || choice.equals("savings") || choice.equals("savings account")) {
 					this.accountType = Account.SAVINGS;
-					checkingPS.println("0");
-					savingsPS.println("1");
-					jointPS.println("0");
 					b = false;
 				} else if (choice.equals("3") || choice.equals("joint") || choice.equals("joint account")) {
 					this.accountType = Account.JOINT;
-					checkingPS.println("0");
-					savingsPS.println("0");
-					jointPS.println("1");
 					b = false;
 				} else {
 					System.out.print("Please enter a valid input: ");
@@ -82,6 +72,8 @@ public class AccountCreationMenu {
 			System.out.println();
 
 			input.close();
+			
+			statement.execute(String.format("INSERT INTO customer VALUES ('Derrick', 'Cheah', %s, %s)", customerId, password));
 
 			this.bankAccount = new BankAccountBuilder().with($ -> {
 				$.accountNumber = AccountCreationMenu.accountNumber;
@@ -96,7 +88,7 @@ public class AccountCreationMenu {
 			accountNumber++;
 
 			System.out.println("Application Pending. Come back later.");
-		} catch (FileNotFoundException e) {
+		} catch (SQLException e) {
 
 		}
 	}
