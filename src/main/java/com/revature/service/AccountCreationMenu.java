5db0d5d5943ad00ae41e7bc78cc4300a3d91f3b0
Derrick Cheah <derrick.cheah01@gmail.com>
2018-12-19 13:01:29 -0600

    Added AWS SQL DB. Modified insertion method.

--- src/main/java/com/revature/service/AccountCreationMenu.java
+++ src/main/java/com/revature/service/AccountCreationMenu.java
@@ -1,13 +1,6 @@
 package com.revature.service;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Scanner;
 
 import com.revature.entity.Account;
@@ -19,78 +12,79 @@ public class AccountCreationMenu {
 	public static int accountNumber = 0;
 	private String customerId;
 	private String password;
+	private String firstName;
+	private String lastName;
 	private Account accountType;
 	private BankAccount bankAccount;
-	private final String url = "jdbc:postgresql://baasu.db.elephantsql.com:5432/mjlaopyp";
-	private final String user = "mjlaopyp";
-	private final String pass = "JSW85gu3KkXAtrdNU20iAbZaVHRgiE6G";
 
 	public AccountCreationMenu() {
 
-		try {
-			
-			Connection connection = DriverManager.getConnection(url, user, pass);
-			Statement statement = connection.createStatement();
-			
-			Scanner input = new Scanner(System.in);
-			System.out.print("Please enter a unique username: ");
-			this.customerId = input.nextLine();
+		Scanner input = new Scanner(System.in);
 
-			System.out.println();
+		System.out.print("Please enter your first name: ");
+		this.firstName = input.nextLine();
 
-			System.out.print("Please enter a password: ");
-			this.password = input.nextLine();
+		System.out.println();
 
-			System.out.println();
+		System.out.print("Please enter your last name: ");
+		this.lastName = input.nextLine();
 
-			System.out.println("What type of account would you like to create?");
-			System.out.println("\t1. Checking Account\n\t2. Savings Account\n\t3. Joint Account");
+		System.out.println();
 
-			String choice = input.nextLine();
-			choice = choice.toLowerCase();
+		System.out.print("Please enter a unique username: ");
+		this.customerId = input.nextLine();
 
-			boolean b = true;
+		System.out.println();
 
-			do {
-				if (choice.equals("1") || choice.equals("checking") || choice.equals("checking account")) {
-					this.accountType = Account.CHECKING;
-					b = false;
-				} else if (choice.equals("2") || choice.equals("savings") || choice.equals("savings account")) {
-					this.accountType = Account.SAVINGS;
-					b = false;
-				} else if (choice.equals("3") || choice.equals("joint") || choice.equals("joint account")) {
-					this.accountType = Account.JOINT;
-					b = false;
-				} else {
-					System.out.print("Please enter a valid input: ");
-					input = new Scanner(System.in);
-					choice = input.nextLine();
-					choice = choice.toLowerCase();
-				}
-			} while (b);
+		System.out.print("Please enter a password: ");
+		this.password = input.nextLine();
 
-			System.out.println();
+		System.out.println();
 
-			input.close();
-			
-			statement.execute(String.format("INSERT INTO customer VALUES ('Derrick', 'Cheah', %s, %s)", customerId, password));
+		System.out.println("What type of account would you like to create?");
+		System.out.println("\t1. Checking Account\n\t2. Savings Account\n\t3. Joint Account");
 
-			this.bankAccount = new BankAccountBuilder().with($ -> {
-				$.accountNumber = AccountCreationMenu.accountNumber;
-				$.customerId = this.customerId;
-				$.password = this.password;
-				$.account = this.accountType;
-			}).buildBankAccount();
+		String choice = input.nextLine();
+		choice = choice.toLowerCase();
 
-			// TODO
-			// Store account information as an arraylist.
+		boolean b = true;
 
-			accountNumber++;
+		do {
+			if (choice.equals("1") || choice.equals("checking") || choice.equals("checking account")) {
+				this.accountType = Account.CHECKING;
+				b = false;
+			} else if (choice.equals("2") || choice.equals("savings") || choice.equals("savings account")) {
+				this.accountType = Account.SAVINGS;
+				b = false;
+			} else if (choice.equals("3") || choice.equals("joint") || choice.equals("joint account")) {
+				this.accountType = Account.JOINT;
+				b = false;
+			} else {
+				System.out.print("Please enter a valid input: ");
+				input = new Scanner(System.in);
+				choice = input.nextLine();
+				choice = choice.toLowerCase();
+			}
+		} while (b);
 
-			System.out.println("Application Pending. Come back later.");
-		} catch (SQLException e) {
+		System.out.println();
 
-		}
+		input.close();
+
+		this.bankAccount = new BankAccountBuilder().with($ -> {
+			$.accountNumber = AccountCreationMenu.accountNumber;
+			$.customerId = this.customerId;
+			$.password = this.password;
+			$.account = this.accountType;
+		}).buildBankAccount();
+
+		accountNumber++;
+
+		CreateAccountDAO dao = new CreateAccountDAO();
+		Connection connection = dao.getConnection();
+		dao.setDBValues(connection, this.firstName, this.lastName, this.customerId, this.password);
+
+		System.out.println("Application Creation Succesful! Please Check Back Later.");
 	}
 
 	public BankAccount getAccount() {
